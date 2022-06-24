024249542ddf0950573eea603d5d7c5ab364ee5c
sugar <engineerzjl@foxmail.com>
2022-06-24 09:55:49 +0800

    chore: 修复方法拼写错误，补全测试用例

---
+++ __tests__/http.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from 'vitest'
+import { getFullURL } from '../src/utils/http'
+
+test('getFullURL1', () => {
+    expect(getFullURL('', '/api', {})).toBe('/api')
+    expect(getFullURL('/', '/api/user', {})).toBe('/api/user')
+
+    expect(getFullURL('/api', 'user', {})).toBe('/api/user')
+    expect(getFullURL('/api', '/user', {})).toBe('/api/user')
+    expect(getFullURL('/api', '/user/login', {})).toBe('/api/user/login')
+    expect(getFullURL('/api', 'user/login', {})).toBe('/api/user/login')
+
+    expect(getFullURL('/api/', 'user', {})).toBe('/api/user')
+    expect(getFullURL('/api/', '/user', {})).toBe('/api/user')
+    expect(getFullURL('/api/', '/user/login', {})).toBe('/api/user/login')
+    expect(getFullURL('/api/', 'user/login', {})).toBe('/api/user/login')
+
+    expect(getFullURL('api/', 'user', {})).toBe('api/user')
+    expect(getFullURL('api/', '/user', {})).toBe('api/user')
+    expect(getFullURL('api/', '/user/login', {})).toBe('api/user/login')
+    expect(getFullURL('api/', 'user/login', {})).toBe('api/user/login')
+
+    expect(getFullURL('http://www.xxx.com', 'user', {})).toBe('http://www.xxx.com/user')
+    expect(getFullURL('http://www.xxx.com', '/user', {})).toBe('http://www.xxx.com/user')
+    expect(getFullURL('http://www.xxx.com/', 'user', {})).toBe('http://www.xxx.com/user')
+    expect(getFullURL('http://www.xxx.com/', '/user', {})).toBe('http://www.xxx.com/user')
+    expect(getFullURL('http://www.xxx.com/', '/user/login', {})).toBe('http://www.xxx.com/user/login')
+    expect(getFullURL('http://www.xxx.com/', 'http://www.xxx.com/user2/login', {})).toBe('http://www.xxx.com/user2/login')
+
+    expect(getFullURL('api/', 'user/getList', { id: 1 })).toBe('api/user/getList?id=1')
+    expect(getFullURL('api/', 'user/getList?id=2', { age: 18 })).toBe('api/user/getList?id=2&age=18')
+    expect(getFullURL('api/', 'user/getList?id=2', { id: 2 })).toBe('api/user/getList?id=2&id=2')
+    expect(getFullURL('api/', 'user/getList?id=2&age=3', { sex: 1 })).toBe('api/user/getList?id=2&age=3&sex=1')
+    expect(getFullURL('api/', 'user/getList?id=2&age=3', { sex: 1 })).toBe('api/user/getList?id=2&age=3&sex=1')
+
+    expect(getFullURL('api/', 'user/getList?id=2&age=3#123', {})).toBe('api/user/getList?id=2&age=3#123')
+
+})
