48a7cbbecef5dc1529c3555d07aad2224e160631
sugar <engineerzjl@foxmail.com>
2022-10-30 22:34:34 +0800

    chore: 根据官方文档示例修改 大菠萝引入方式

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createSSRApp } from 'vue'
-import { createPinia } from 'pinia'
+import * as Pinia from 'pinia'
 // @ts-ignore
 import uView from 'vk-uview-ui'
 import App from './App.vue'
 
 export function createApp() {
   const app = createSSRApp(App)
-  app.use(createPinia())
+  app.use(Pinia.createPinia())
   app.use(uView)
   return {
-    app
+    app,
+    // uni-app 官方文档示例 https://zh.uniapp.dcloud.io/tutorial/vue3-pinia.html#%E7%8A%B6%E6%80%81%E7%AE%A1%E7%90%86-pinia
+    Pinia // 此处必须将 Pinia 返回
   }
 }
