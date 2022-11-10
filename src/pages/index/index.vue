a654828bc8eeda430381d213d258103005f47431
MellowCo <799478052@qq.com>
2022-11-10 21:45:27 +0800

    feat(components): add unocss demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="index-page">
     <Hello />
+    <UnoCss />
     <text class="h2"> 查看其它页面↓ </text>
     <view>
       <navigator v-for="(v, idx) in pages" :key="idx" :url="v.url">{{
@@ -13,6 +14,7 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import Hello from '@/components/hello/index.vue'
+import UnoCss from '@/components/unocss/index.vue'
 
 const pages = reactive([
   {
