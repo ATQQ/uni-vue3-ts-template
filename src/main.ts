435d0cfb6c73c32a86e97ae5b5b7d2bc7c7d7079
MellowCo <799478052@qq.com>
2022-11-10 21:42:04 +0800

    chore(config): add unocss config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import * as Pinia from 'pinia'
 import uView from 'vk-uview-ui'
 import App from './App.vue'
 
+// unocss
+import 'uno.css'
+
 export function createApp() {
   const app = createSSRApp(App)
   app.use(Pinia.createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import path from 'path'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    // https://github.com/antfu/unocss
+    Unocss()
+  ],
   server: {
     // port: 8080,
     host: '0.0.0.0',
