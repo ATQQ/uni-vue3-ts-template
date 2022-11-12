0d532725838be84fd95092e5078c24922452bfde
sugar <engineerzjl@foxmail.com>
2022-11-12 15:18:57 +0800

    fix: url 图片路径问题

--- src/components/unocss/index.vue
+++ src/components/unocss/index.vue
@@ -7,7 +7,7 @@
       animate-iteration-infinite
       animate-swing
       bg-cover
-      class="bg-[url(https://raw.githubusercontent.com/unocss/unocss/main/playground/public/icon-gray.svg)]"
+      class="bg-[url(https://img.cdn.sugarat.top/mdImg/MTY2ODA4OTc3MjcyMg==unocss-icon-gray.svg)]"
     ></view>
 
     <view class="bg-#3498db/20 text-#3498db rounded" p="x2 y1"
